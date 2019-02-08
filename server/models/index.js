cf4bbaec479a067005015ac5e3421e5e0864d76e
Mike <mikeglopez.91@gmail.com>
2019-02-08 00:00:57 -0600

    Begin persistent SQL-backed server

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,62 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      var queryString = 'SELECT * FROM messages';
+      var queryArgs = [];
+
+      dbConnection.query(queryString, queryArgs, function (err, results) {
+        if (err) {
+          console.log('error==================>', err);
+          // res.sendStatus(404);
+        } else {
+          console.log('SUCCESS withing GET MESSAGE');
+          // res.status(200).json({});
+        }
+      }
+      );
+    }, // a function which produces all the messages
+    post: function (message) {
+      var query = dbConnection.query('INSERT INTO messages (message_text) VALUES ?', message, function (error, results, fields) {
+        if (error) {
+          throw error;
+        } else {
+          console.log('SUCCESS within POST MESSAGE');
+          // Neat!
+          // res.status(201).json({objectId, createdAt}});
+        }
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () {
+      var queryString = 'SELECT * FROM users';
+      var queryArgs = [];
+
+      dbConnection.query(queryString, queryArgs, function (err, results) {
+        if (err) {
+          console.log('error==================>', err);
+          // res.sendStatus(404);
+        } else {
+          console.log('SUCCESS within GET USER');
+          // res.status(200).json({});
+        }
+      }
+      );
+    },
+    post: function (user) {
+      var query = dbConnection.query('INSERT INTO users (user_name) VALUES ?', user, function (error, results, fields) {
+        if (error) {
+          throw error;
+        } else {
+          console.log('SUCCESS within POST USER');
+          // Neat!
+          // res.status(201).json({objectId, createdAt}});
+        }
+      });
+    }
   }
 };
 
